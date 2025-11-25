8e06cfee47a9ebe9605834b363905e1e084ed67f
nicolashs32 <nicolashs32.32@gmail.com>
2025-11-25 19:43:11 +0000

    Agrego Punto A y Punto B completos + estructura de animales

---
+++ RTA_Examen_20251121/RTA_Examen_20251121/Punto_B.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##script utilizado para el punto B
+##clasificacion de animales segun habitat
+
+
+mkdir -p /tmp/Animales
+mkdir -p /tmp/Animales/Agua
+mkdir -p /tmp/Animales/Tierra/Mamiferos
+mkdir -p /tmp/Animales/Oviparos
+
+echo "" > /tmp/animales.txt
+
+archivo="$1"
+
+while IFS=';' read -r animal habitat; do
+
+	if [[ -z "$animal" ]] || [[ "$animal" =~ ^# ]]; then
+		continue
+	fi
+
+	fecha="$(date +%Y%m%d)"
+	linea="$fecha - animal: $animal - habitat: $habitat"
+
+	echo "$linea" >> /tmp/animales.txt
+
+	case "$habitat" in
+		AG)
+	echo "$linea" >> "/tmp/Animales/Agua/${animal}.txt"
+	;;
+		TM)
+	echo "$linea" >> "/tmp/Animales/Tierra/Mamiferos/${animal}.txt"
+	;;
+		TO)
+	echo "$linea" >> "/tmp/Animales/Oviparos/${animal}.txt"
+	;;
+	esac
+
+done < "$archivo"
